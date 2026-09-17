48a6a539d7c8384d24c68b934f4f0a4903fae558
ferreira-gn <geanoficial14@gmail.com>
2026-09-17 15:34:18 -0300

    style: prettier all entitys

--- src/main/java/com/ferreira_gn/dsc/entity/UserEntity.java
+++ src/main/java/com/ferreira_gn/dsc/entity/UserEntity.java
@@ -1,11 +1,9 @@
 package com.ferreira_gn.dsc.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.util.Date;
